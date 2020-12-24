06f51e3330cdfcd020f385c8e55e44bbea1e601d
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-24 17:27:49 +0000

    trying to set up 1st route Music

--- src/components/Music.js
+++ src/components/Music.js
@@ -8,8 +8,8 @@ export default class Music extends Component{
 				<h1>Music stuff ok?</h1>
 				<p>
 					<br/><br/><br/>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</p>
 			</div>
 		)
