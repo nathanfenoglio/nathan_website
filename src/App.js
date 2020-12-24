ec2a9c2606a7a2842dff420f47f4f5d348f8371c
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-24 15:47:39 +0000

    starting to work on routing for navbar options, adding import statement

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-		<Navbar />
-      <header className="App-header">
-		<p>
-			<br/><br/><br/>
-		  <iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		</p>
-        <a>Flamm</a>
-      </header>
-    </div>
-  );
+//function App() {
+class App extends Component{
+	render(){
+		  return (
+			<div className="App">
+				<Navbar />
+			  <header className="App-header">
+				<p>
+					<br/><br/><br/>
+				  <iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</p>
+				<a>Flamm</a>
+			  </header>
+			</div>
+		  );
+	}
 }
 
 export default App;
