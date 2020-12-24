e75d0277acc6d4a1f9eb6a3b8b84fbd0d734ae83
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-24 16:39:13 +0000

    trying to set up 1st route Music

--- src/App.js
+++ src/App.js
@@ -15,11 +15,13 @@ class App extends Component{
 					<br/>
 					<Route path="/Music" exact component={Music} />
 					  <header className="App-header">
+					  {/*
 						<p>
 							<br/><br/><br/>
 						  <iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 							<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 						</p>
+					  */}
 						<a>Flamm</a>
 					  </header>
 				</div>
--- src/components/Music.js
+++ src/components/Music.js
@@ -6,6 +6,11 @@ export default class Music extends Component{
 		return(
 			<div>
 				<h1>Music stuff ok?</h1>
+				<p>
+					<br/><br/><br/>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				</p>
 			</div>
 		)
 	}
