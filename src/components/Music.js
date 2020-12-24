baab3af90b32eb2f4bd6cc16dd35d3d8baab366c
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-24 17:30:06 +0000

    trying to set up 1st route Music

--- src/components/Music.js
+++ src/components/Music.js
@@ -9,6 +9,7 @@ export default class Music extends Component{
 				<p>
 					<br/><br/><br/>
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<br/>
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</p>
 			</div>
