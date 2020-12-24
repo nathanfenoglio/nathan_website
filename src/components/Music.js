342ca70d463b217a45e3fcd1bcbe6f3e3253b339
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-24 17:35:52 +0000

    trying to set up 1st route Music

--- src/components/Music.js
+++ src/components/Music.js
@@ -13,6 +13,19 @@ export default class Music extends Component{
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</p>
 			</div>
+			<div>
+				<h1>Harry Has a Head Like a Ping Pong Ball</h1>
+				<p>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/nUqgGL8950c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<br/>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/AkpB8N6afDw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<br/>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/QN8Dvm9QMUU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<br/>
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/apTxH9tEXhY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					<br/>
+				</p>
+			</div>
 		)
 	}
 }
