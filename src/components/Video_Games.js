1fa994589ae7c7bf9de1e3d53677112fe63f9a84
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-25 15:05:47 +0000

    Working on Video Games page

--- src/components/Video_Games.js
+++ src/components/Video_Games.js
@@ -9,7 +9,7 @@ export default class Video_Games extends Component{
 				<h1>Escape From Underground Terrestrial Abyss</h1>
 				
 				<h3>Cubeland Solution Path</h3>
-				<Link to="/Cubeland_Solution_Path">Cubeland Solution Path</Link>
+				<Link to="/components/Cubeland_Solution_Path">Cubeland Solution Path</Link>
 				{/*
 				<Link to="/Cubeland_Solution_Path.mht">Cubeland Solution Path</Link>
 				<iframe className="docx" width="100%" height="600" frameborder="0" src="/Cubeland_Solution_Path.docx"></iframe>
