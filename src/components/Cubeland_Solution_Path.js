f1d8195304669fb8c0915d87932a26eb92d6a90f
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-25 15:03:58 +0000

    Working on Video Games page

--- src/components/Cubeland_Solution_Path.js
+++ src/components/Cubeland_Solution_Path.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
-export default class Video_Games extends Component{
+export default class Cubeland_Solution_Path extends Component{
 	render(){
 		return(
 			<div>
-				<h1>Video Game Stuff ok?</h1>
-				<h1>Escape From Underground Terrestrial Abyss</h1>
-				
-				<h3>Cubeland Solution Path</h3>
-				<Link to="/Cubeland_Solution_Path">Cubeland Solution Path</Link>
-				{/*
-				<Link to="/Cubeland_Solution_Path.mht">Cubeland Solution Path</Link>
-				<iframe className="docx" width="100%" height="600" frameborder="0" src="/Cubeland_Solution_Path.docx"></iframe>
-				*/}
-				
-				{/*<iframe src= "./Cubeland_Solution_Path.mht" width="100%" height="800"> </iframe>*/}
 				<p style={{textAlign: 'left'}}><strong>SPOILER ALERT </strong></p>
         <p style={{textAlign: 'left'}}>(If you find yourself lost and don’t have the time to ponder the cube to get your keys back)</p>
         <p style={{textAlign: 'left'}}>&nbsp;</p>
