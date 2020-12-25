eefec0ce404856f55fe3da8570beb5095f182451
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-25 14:28:03 +0000

    adding Video_Games page

--- src/components/Video_Games.js
+++ src/components/Video_Games.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-var file_url = "Cubeland_Solution_Path.docx";
-var filetype = ".docx";
+
 export default class Video_Games extends Component{
 	render(){
 		return(
@@ -10,8 +9,11 @@ export default class Video_Games extends Component{
 				<h1>Escape From Underground Terrestrial Abyss</h1>
 				
 				<h3>Cubeland Solution Path</h3>
+				{/*
 				<Link to="/Cubeland_Solution_Path.mht">Cubeland Solution Path</Link>
 				<iframe className="docx" width="100%" height="600" frameborder="0" src="/Cubeland_Solution_Path.docx"></iframe>
+				*/}
+				<iframe src= "Cubeland_Solution_Path.mht" width="100%" height="800"> </iframe>
 			</div>
 		)
 	}
