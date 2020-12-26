685d0d295de5cda5c4e31d8100fb08bd72f8e732
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-26 01:24:27 +0000

    add a slideshow to App.js

--- src/App.js
+++ src/App.js
@@ -56,6 +56,26 @@ class App extends Component{
 					  
 						<a>Flamm</a>
 					  </header>
+					  
+					<div className="slide-container">
+						<Slide {...properties}>
+							<div className="each-slide">
+								<div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+									<span>Slide 1</span>
+								</div>
+							</div>
+							<div className="each-slide">
+								<div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+									<span>Slide 2</span>
+								</div>
+							</div>
+							<div className="each-slide">
+								<div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+									<span>Slide 3</span>
+								</div>
+							</div>
+						</Slide>
+					</div>
 				</div>
 			</Router>
 		  );
