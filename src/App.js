811cb86ba01e5e1c1c19ec263a738b4c207bcfdc
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-26 02:41:58 +0000

    still trying to get slideshow going

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ import Code_Projects from "./components/Code_Projects";
 import Web_Apps from "./components/Web_Apps";
 import Misc from "./components/Misc";
 
+import image_1 from "/My_Face_186.png"
 const slideImages = [
-  '/src/My_Face_186.PNG',
+  '/src/My_Face_186.png',
   '/src/My_Face_187.png',
   '/src/My_Face_188.png'
 ];
@@ -23,7 +24,7 @@ const Slideshow = () => {
       <div className="slide-container">
         <Slide>
           <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            <div style={{'backgroundImage': {image_1}}}>
               <span>Slide 1</span>
             </div>
           </div>
