2158910d53b0fac474a4243ea7194b98b58c7f8c
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-26 01:44:44 +0000

    add a slideshow to App.js

--- src/components/Music.js
+++ src/components/Music.js
@@ -10,7 +10,7 @@ export default class Music extends Component{
 	render(){
 		return(
 			<div>
-				<h1>Music stuff ok?</h1>
+				<h1>Music stuff</h1>
 				<p>
 					<br/><br/><br/>
 					<iframe width="560" height="315" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
