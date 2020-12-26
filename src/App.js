3525985461c31333b19c42978ca6d4d506dc99ed
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2020-12-26 14:14:38 +0000

    adding slideshow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Carousel from "react-bootstrap/Carousel";
 
 import Music from "./components/Music";
 import Video_Games from "./components/Video_Games";
@@ -9,6 +10,14 @@ import Cubeland_Solution_Path from "./components/Cubeland_Solution_Path";
 import Code_Projects from "./components/Code_Projects";
 import Web_Apps from "./components/Web_Apps";
 import Misc from "./components/Misc";
+
+import image_1 from "./images/green.jpg";
+import image_2 from "./images/yellow.jpg";
+import image_3 from "./images/red.jpg";
+import image_4 from "./images/blue.jpg";
+import image_5 from "./images/orange.jpg";
+import image_6 from "./images/white.jpg";
+
 
 //function App() {
 class App extends Component{
@@ -37,6 +46,80 @@ class App extends Component{
 						
 						<a>Flamm</a>
 					  </header>
+						<Carousel interval={1000}>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src={image_1}
+									alt=""
+									style={{ height: "100vh"}}
+								/>
+								<Carousel.Caption>
+									<h3>First Slide Label</h3>
+									<p>Scenery 1</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src={image_2}
+									alt=""
+									style={{ height: "100vh" }}
+								/>
+								<Carousel.Caption>
+									<h3>Second Slide Label</h3>
+									<p>Scenery 2</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src={image_3}
+									alt=""
+									style={{ height: "100vh" }}
+								/>
+								<Carousel.Caption>
+									<h3>Third Slide Label</h3>
+									<p>Scenery 3</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src={image_4}
+									alt=""
+									style={{ height: "100vh" }}
+								/>
+								<Carousel.Caption>
+									<h3>Fourth Slide Label</h3>
+									<p>Scenery 4</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src={image_5}
+									alt=""
+									style={{ height: "100vh" }}
+								/>
+								<Carousel.Caption>
+									<h3>Fifth Slide Label</h3>
+									<p>Scenery 5</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+							<Carousel.Item>
+								<img
+									className="d-block w-100"
+									src={image_6}
+									alt=""
+									style={{ height: "100vh" }}
+								/>
+								<Carousel.Caption>
+									<h3>Sixth Slide Label</h3>
+									<p>Scenery 6</p>
+								</Carousel.Caption>
+							</Carousel.Item>
+						</Carousel>
 					
 				</div>
 			</Router>
