ffdcb009eda5eb782e934dec036219a8f33f8ac2
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2021-01-05 13:36:20 -0600

    changing backgrounds and stuff

--- src/components/Music.js
+++ src/components/Music.js
@@ -14,105 +14,115 @@ export default class Music extends Component{
 				{window.scrollTo(0, 0)}
 				<h1 className="page-title">Musical Pursuits</h1>
 				<br/><br/><br/>
-				<h1 className="band">Solo Electronic Music</h1>
-				<p>
+
+				<div style={{ backgroundColor: "cyan", width: "60vw", marginLeft: "20vw", marginBottom: "5vw", paddingTop: "5vw"}}>
+					<h1 className="band">Solo Electronic Music</h1>
+					<p>
+						<br/>
+						<h3 className="song-title" className="song-title">Time to Travel, Face It</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/><br/><br/>
+						<h3 className="song-title">This Is It</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/><br/><br/>
+						<h3 className="song-title">A Lightbulb Attached to His Head Lights Up</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/q8iOnHRrHpk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/><br/><br/>
+						<h3 className="song-title">Let's Have a Look In Here Why Don't We</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/eKb4dW4DtT4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/><br/><br/>
+						<h3 className="song-title">Rubiks Cuban</h3>
+						<iframe className="audio-link"
+						frameborder="0"
+						width="50vw"
+						height="152"
+						src="https://drive.google.com/file/d/15CwM3d4vTSsdbxBjLVnU_ZIdm0irdMwN/preview?usp=sharing">
+						</iframe>
+						<br/><br/>
+					</p>
+				</div>
+
+				<div style={{ backgroundColor: "limegreen", width: "60vw", marginLeft: "20vw", marginBottom: "8vw", paddingBottom: "5vw", paddingTop: "5vw"}}>
+					<h1 className="band">Harry Has a Head Like a Ping Pong Ball</h1>
+					<h2 className="album-title">Post Apocalyptic Time Travel Core</h2>
+					<ul className="players">
+						<li>Guitar: Travis Laminack</li>
+						<li>Drums: Dustin Adams</li>
+						<li>Bass: Nathan Fenoglio</li>
+					</ul>
+					<p>
+						<br/>
+						<h3 className="song-title">Skullhead</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/nUqgGL8950c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/>
+						<h3 className="song-title">Rootbeerrun</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/AkpB8N6afDw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/>
+						<h3 className="song-title">Walter Cronkite Goes Fishin'</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/QN8Dvm9QMUU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/>
+						<h3 className="song-title">Send in Mcgraw</h3>
+						<iframe className="youtube-link" src="https://www.youtube.com/embed/apTxH9tEXhY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+						<br/>
+					</p>
+					
+					<h2 className="album-title">Live at Andy's Denton, TX a Long Time Ago</h2>
+					<h4 className="players" style={{color: "white"}}>2 Unreleased Tracks: </h4>
+					<li className="players" style={{color: "white"}}>Get That Loot Into the Bag as Reasonably Quick as You Can Let's go Hey</li>
+					<li className="players" style={{color: "white"}}>Stealing Crackers From the Wheat Farm</li>
+					<ul className="players">
+						<li>Guitar: Travis Laminack</li>
+						<li>Drums: Gabriel Pearson</li>
+						<li>Bass: Nathan Fenoglio</li>
+					</ul>
 					<br/>
-					<h3 className="song-title" className="song-title">Time to Travel, Face It</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/jTS6Jh6uF40" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/><br/><br/>
-					<h3 className="song-title">This Is It</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/5Vk0QhhUjtM" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/><br/><br/>
-					<h3 className="song-title">A Lightbulb Attached to His Head Lights Up</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/q8iOnHRrHpk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/><br/><br/>
-					<h3 className="song-title">Let's Have a Look In Here Why Don't We</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/eKb4dW4DtT4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/><br/><br/>
-					<h3 className="song-title">Rubiks Cuban</h3>
-					<iframe className="audio-link"
-					   frameborder="0"
-					   width="50vw"
-					   height="152"
-					   src="https://drive.google.com/file/d/15CwM3d4vTSsdbxBjLVnU_ZIdm0irdMwN/preview?usp=sharing">
-					</iframe>
-					<br/><br/><br/>
-				</p>
-			
-				<h1 className="band">Harry Has a Head Like a Ping Pong Ball</h1>
-				<h2 className="album-title">Post Apocalyptic Time Travel Core</h2>
-				<ul className="players">
-					<li>Guitar: Travis Laminack</li>
-					<li>Drums: Dustin Adams</li>
-					<li>Bass: Nathan Fenoglio</li>
-				</ul>
-				<p>
+					<h3 className="song-title">Get That Loot Into the Bag as Reasonably Quick as You Can Let's go Hey</h3>
+					<audio className="audio-link" ref="audio_tag" src={LiveAtAndysTrack1} controls />
 					<br/>
-					<h3 className="song-title">Skullhead</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/nUqgGL8950c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/>
-					<h3 className="song-title">Rootbeerrun</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/AkpB8N6afDw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/>
-					<h3 className="song-title">Walter Cronkite Goes Fishin'</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/QN8Dvm9QMUU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/>
-					<h3 className="song-title">Send in Mcgraw</h3>
-					<iframe className="youtube-link" src="https://www.youtube.com/embed/apTxH9tEXhY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-					<br/>
-				</p>
-				
-				<h2 className="album-title">Live at Andy's Denton, TX a Long Time Ago</h2>
-				<h4 className="players" style={{color: "white"}}>2 Unreleased Tracks: </h4>
-				<li className="players" style={{color: "white"}}>Get That Loot Into the Bag as Reasonably Quick as You Can Let's go Hey</li>
-				<li className="players" style={{color: "white"}}>Stealing Crackers From the Wheat Farm</li>
-				<ul className="players">
-					<li>Guitar: Travis Laminack</li>
-					<li>Drums: Gabriel Pearson</li>
-					<li>Bass: Nathan Fenoglio</li>
-				</ul>
-				<br/>
-				<h3 className="song-title">Get That Loot Into the Bag as Reasonably Quick as You Can Let's go Hey</h3>
-				<audio className="audio-link" ref="audio_tag" src={LiveAtAndysTrack1} controls />
-				<br/>
-				<h3 className="song-title">Stealing crackers from the wheat farm</h3>
-				<audio className="audio-link" ref="audio_tag" src={LiveAtAndysTrack2} controls/>
-				
-				<h1 className="band" style={{fontSize: "8vw"}}>Parallel Realities</h1>
-				<h2 className="album-title">Self Titled</h2>
-				<ul className="players">
-					<li>Vocals: Rocio Herrera</li>
-					<li>Drum Set: Garrett Gross</li>
-					<li>Auxiliary Percussion: Eric Rogers</li>
-					<li>Keys: Sean Pollaro</li>
-					<li>Guitar: Chris Belknap</li>
-					<li>Bass: Nathan Fenoglio</li>
-				</ul>
-				<br/><br/>
-				<h3 className="song-title">Reflection</h3>
-				<audio className="audio-link" ref="audio_tag" src={ParallelRealities_Reflection} controls/>
-				<br/><br/>
-				<h3 className="song-title">Give Me One Good Reason</h3>
-				<audio className="audio-link" ref="audio_tag" src={ParallelRealities_GiveMeOneGoodReason} controls/>
-				<br/><br/>
-				<h3 className="song-title">Heartbeat</h3>
-				<audio className="audio-link" ref="audio_tag" src={ParallelRealities_Heartbeat} controls/>
-				<br/><br/>
-				
-				<h2 className="album-title">Live at Haileys Denton, TX 2005</h2>
-				<ul className="players">
-					<li>Vocals: Rocio Herrera</li>
-					<li>Drum Set: Garrett Gross</li>
-					<li>Auxiliary Percussion: Eric Rogers</li>
-					<li>Keys: Sean Pollaro</li>
-					<li>Guitar: Chris Belknap</li>
-					<li>Bass: Nathan Fenoglio</li>
-				</ul>
-				<br/><br/>
-				<iframe className="youtube-link" src="https://www.youtube.com/embed/Zp5jtLA8D2I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-				
-				<br/><br/><br/><br/>
-				<h1 className="band" style={{fontSize: "8vw"}}> Wrought of Obsidian</h1>
+					<h3 className="song-title">Stealing crackers from the wheat farm</h3>
+					<audio className="audio-link" ref="audio_tag" src={LiveAtAndysTrack2} controls/>
+				</div>
+
+				<div style={{ backgroundColor: "peru", width: "60vw", marginLeft: "20vw", marginBottom: "8vw", paddingBottom: "5vw", paddingTop: "5vw"}}>
+					<h1 className="band" style={{fontSize: "8vw"}}>Parallel Realities</h1>
+					<h2 className="album-title">Self Titled</h2>
+					<ul className="players">
+						<li>Vocals: Rocio Herrera</li>
+						<li>Drum Set: Garrett Gross</li>
+						<li>Auxiliary Percussion: Eric Rogers</li>
+						<li>Keys: Sean Pollaro</li>
+						<li>Guitar: Chris Belknap</li>
+						<li>Bass: Nathan Fenoglio</li>
+					</ul>
+					<br/><br/>
+					<h3 className="song-title">Reflection</h3>
+					<audio className="audio-link" ref="audio_tag" src={ParallelRealities_Reflection} controls/>
+					<br/><br/>
+					<h3 className="song-title">Give Me One Good Reason</h3>
+					<audio className="audio-link" ref="audio_tag" src={ParallelRealities_GiveMeOneGoodReason} controls/>
+					<br/><br/>
+					<h3 className="song-title">Heartbeat</h3>
+					<audio className="audio-link" ref="audio_tag" src={ParallelRealities_Heartbeat} controls/>
+					<br/><br/>
+					
+					<h2 className="album-title">Live at Haileys Denton, TX 2005</h2>
+					<ul className="players">
+						<li>Vocals: Rocio Herrera</li>
+						<li>Drum Set: Garrett Gross</li>
+						<li>Auxiliary Percussion: Eric Rogers</li>
+						<li>Keys: Sean Pollaro</li>
+						<li>Guitar: Chris Belknap</li>
+						<li>Bass: Nathan Fenoglio</li>
+					</ul>
+					<br/><br/>
+					<iframe className="youtube-link" src="https://www.youtube.com/embed/Zp5jtLA8D2I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+					
+					<br/><br/><br/><br/>
+				</div>
+
+				<div style={{ backgroundColor: "black", width: "60vw", marginLeft: "20vw", marginBottom: "8vw", paddingBottom: "5vw", paddingTop: "5vw"}}>
+					<h1 className="band" style={{fontSize: "8vw"}}> Wrought of Obsidian</h1>
+				</div>
 			</div>
 		)
 	}
--- src/components/Video_Games.js
+++ src/components/Video_Games.js
@@ -13,25 +13,31 @@ export default class Video_Games extends Component{
 				<h1 className="game-title">Escape From Underground Terrestrial Abyss</h1>
 				<img className="game-static-image" src={title_screenshot_escape_from_uta}/>
 				<h2 className="play-game">Play Game!!! I compel you</h2>
-				<p className="directions">Click On One Of The Links Below</p>
-				<a className="links" target="_blank" rel="noopener noreferrer" href="https://simmer.io/@nathanfenoglio/escape-from-underground-terrestrial-abyss">Play Game With Keyboard Controls</a>
-				<br/>
-				{/*<iframe src="https://i.simmer.io/@nathanfenoglio/escape-from-underground-terrestrial-abyss" style={{width: "960px", height: "600px"}}></iframe>*/}
-				<a className="links" target="_blank" rel="noopener noreferrer" href="https://simmer.io/@nathanfenoglio/escape-from-underground-terrestrial-abyss-mobile-controls">Play Game With Touch Screen Controls</a>
+				
+				<div style={{ backgroundColor: "black", paddingTop: "2vw", paddingBottom: "5vw" }}>
+					<p className="directions">Click On One Of The Links Below</p>
+					<a className="links" target="_blank" rel="noopener noreferrer" href="https://simmer.io/@nathanfenoglio/escape-from-underground-terrestrial-abyss">Play Game With Keyboard Controls</a>
+					<br/>
+					{/*<iframe src="https://i.simmer.io/@nathanfenoglio/escape-from-underground-terrestrial-abyss" style={{width: "960px", height: "600px"}}></iframe>*/}
+					<a className="links" target="_blank" rel="noopener noreferrer" href="https://simmer.io/@nathanfenoglio/escape-from-underground-terrestrial-abyss-mobile-controls">Play Game With Touch Screen Controls</a>
+				</div>
 
-				<h4 className="dev-team-title">Development Team:</h4>
-				<li className="dev-team-members">Samuel Schreiner</li>
-				<li className="dev-team-members">Hoang Ho</li>
-				<li className="dev-team-members">Hieu Vu</li>
-				<li className="dev-team-members">Debbie Nguyen</li>
-				<li className="dev-team-members">Nathan Fenoglio</li>
-				<br/>
-				
-				<h3 className="cubeland-sol-path">Cubeland Solution Path</h3>
-				<h3 className="spoiler-alert">SPOILER ALERT</h3>
-				<h4 style={{color: 'white', fontSize: '3vw'}}>If you find yourself lost and don’t have the time to ponder the cube to get your keys back</h4>
-				<Link to="/Cubeland_Solution_Path" className="view-solution">View Cubeland Solution Path</Link>
-				
+				<div style={{ backgroundColor: "black", width: "40vw", marginLeft: "30vw"}}>
+					<h4 className="dev-team-title">Development Team:</h4>
+					<li className="dev-team-members">Samuel Schreiner</li>
+					<li className="dev-team-members">Hoang Ho</li>
+					<li className="dev-team-members">Hieu Vu</li>
+					<li className="dev-team-members">Debbie Nguyen</li>
+					<li className="dev-team-members">Nathan Fenoglio</li>
+					
+				</div>
+
+				<div style={{ backgroundColor: "black", marginBottom: "10vw", paddingTop: "0vw", paddingBottom: "5vw" }}>
+					<h3 className="cubeland-sol-path">Cubeland Solution Path</h3>
+					<h3 className="spoiler-alert">SPOILER ALERT</h3>
+					<h4 style={{color: 'white', fontSize: '3vw'}}>If you find yourself lost and don’t have the time to ponder the cube to get your keys back</h4>
+					<Link to="/Cubeland_Solution_Path" className="view-solution">View Cubeland Solution Path</Link>
+				</div>
 			</div>
 		)
 	}
