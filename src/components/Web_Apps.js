2cdd7d33041ed4ae0e294ad037f709ed7ed6603f
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2022-01-01 16:24:23 +0000

    adding cube map to Web_Apps.js

--- src/components/Web_Apps.js
+++ src/components/Web_Apps.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Web_Apps.css';
 import cube_maze_image from "../images/CubeMazeImage.jpg";
+import cube_map_image from "../images/CubeMazeMap.jpg";
 
 export default class Web_Apps extends Component{
 	render(){
@@ -12,9 +13,11 @@ export default class Web_Apps extends Component{
 				<div style={{ backgroundColor: "black", paddingTop: "2vw", paddingBottom: "5vw" }}>
 					<p className="directions">3D Folded Cube Maze</p>
 					<p className="comments">for play on desktop with keyboard arrow controls</p>
+					<a className="links" target="_blank" rel="noopener noreferrer" href="https://nathanfenoglio.github.io/cube_maze/">Play 3D Folded Cube Maze</a>
+					<p className="comments">HERE'S A MAP OF THE MAZE</p>
+					<img className="game-static-image" src={cube_map_image}/>
 					<img className="game-static-image" src={cube_maze_image}/>
 					<br/>
-					<a className="links" target="_blank" rel="noopener noreferrer" href="https://nathanfenoglio.github.io/cube_maze/">Play 3D Folded Cube Maze</a>
 					<br/>
 				</div>
 
