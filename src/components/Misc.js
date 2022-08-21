c7cb7ab6856ff6aa6043c3fe05d9686948f9882b
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2022-08-21 15:10:56 +0100

    messing with style stuff on Music page more

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -7,12 +7,16 @@ export default class Misc extends Component{
 			<div className="misc-container">
 				{window.scrollTo(0, 0)}
 				<h1 className="page-title">Misc</h1>
-				<p className="comments">check out code on github</p>
-				<a className="links" target="_blank" rel="noopener noreferrer" href="https://github.com/nathanfenoglio">GitHub</a>
-				<br/>
-				<p className="comments">check out youtube channel</p>
-				<a className="links" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCu2L2DwJfQsSkZdv3GtGwzQ/videos">YouTube</a>
-				<br/>
+				<div>
+					<p className="comments">check out code on github</p>
+					<a className="links" target="_blank" rel="noopener noreferrer" href="https://github.com/nathanfenoglio">GitHub</a>
+					<br/><br/><br/><br/>
+				</div>
+				<div>
+					<p className="comments">check out youtube channel</p>
+					<a className="links" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/channel/UCu2L2DwJfQsSkZdv3GtGwzQ/videos">YouTube</a>
+					<br/>
+				</div>
 			</div>
 		)
 	}
--- src/components/Web_Apps.js
+++ src/components/Web_Apps.js
@@ -17,12 +17,12 @@ export default class Web_Apps extends Component{
 					<a className="links" target="_blank" rel="noopener noreferrer" href="https://nathanfenoglio.github.io/multi_dimensional_mazes_with_3D_rotations/">Play Multidimensional Mazes With 3D Rotations</a>
 					<br/>
 					<img className="game-static-image" src={multi_d_mazes_with_3D_rotations_image}/>					
-					<p className="comments">Play generated 3D mazes represented as a series of connected 2D planes</p>
-					<p className="comments">Ability to rotate player's perspective up/down left/right in/out <br/>as you attempt to solve each maze</p>
-					<p className="comments">Keyboard Controls:<br/>UP ARROW - up<br/>DOWN ARROW - down<br/>LEFT ARROW - left<br/>RIGHT ARROW - right<br/>ENTER - in<br/>SHIFT - out<br/></p>
-					<p className="comments">w - rotate perspective up<br/>s - rotate perspective down<br/>a - rotate perspective left<br/>d - rotate perspective right<br/><br/></p>
-					<p className="comments">Cells' Color Codes:</p>
-					<p className="comments">Purple Cell - can go in<br/>Green Cell - can go out<br/>Orange Cell - can go in/out<br/>Pink Cell - no in/out</p>
+					<p className="comments" style={{fontSize: "4vw"}}>Play generated 3D mazes represented as a series of connected 2D planes</p>
+					<p className="comments" style={{fontSize: "4vw"}}>Ability to rotate player's perspective up/down left/right in/out <br/>as you attempt to solve each maze</p>
+					<p className="comments" style={{fontSize: "4vw"}}>Keyboard Controls:<br/>UP ARROW - up<br/>DOWN ARROW - down<br/>LEFT ARROW - left<br/>RIGHT ARROW - right<br/>ENTER - in<br/>SHIFT - out<br/></p>
+					<p className="comments" style={{fontSize: "4vw"}}>w - rotate perspective up<br/>s - rotate perspective down<br/>a - rotate perspective left<br/>d - rotate perspective right<br/><br/></p>
+					<p className="comments" style={{fontSize: "4vw"}}>Cells' Color Codes:</p>
+					<p className="comments" style={{fontSize: "4vw"}}>Purple Cell - can go in<br/>Green Cell - can go out<br/>Orange Cell - can go in/out<br/>Pink Cell - no in/out</p>
 
 
 
